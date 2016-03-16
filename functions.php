0d20e66fe37d8748d3562d5ebe081af73f4d8340
lancelynyrd <pinedaclp@gmail.com>
2016-03-16 17:54:17 +0800

    merging changes

--- functions.php
+++ functions.php
@@ -25,6 +25,18 @@ function hd() {
     echo esc_url( home_url( '/' ) );
 }
 
+/**
+ * Get the Id of the ancestor page
+ */
+function get_top_ancestor_id() {
+    global $post;
+    if ($post->post_parent) {
+        $ancestors = array_reverse(get_post_ancestors($post->ID));
+        return $ancestors[0];
+    }
+    return $post->ID;
+}
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
@@ -36,9 +48,22 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'theme', get_template_directory_uri() . '/css/theme.css' );
 }
 
+// if page has children
+function has_children() {
+    global $post;
+    $pages = get_pages('child_of=' . $post->ID);
+    return count($pages);
+}
+
+// Theme Setup
+function wordPressFeatureSetup() {
+    // adding feature image support
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'wordPressFeatureSetup');
 
 
 add_shortcode('intro', function ( ) {
     include get_template_directory() . '/page/intro.php';
 });
-
